982e6c4251258e1f718707dd62ef21550b262f60
Carina Ullman <carinalullman@gmail.com>
2019-09-28 19:53:29 -0600

    First Stuffs and Stuffs

---
+++ script.js
@@ -0,0 +1,58 @@
+
+//create arrays for each Character
+
+var specialChar = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//var hello = prompt("Please choose a number between 8 and 128 to determine the length of your password");
+//if (hello != null) {
+
+
+
+//function getRandomInt(max) {
+  //return Math.floor(Math.random() * Math.floor(max));}
+
+
+var password = "";
+/*console.log(password);
+password = "abc";
+console.log(password);
+password = password + "def";
+console.log(password);*/
+
+for (var i=0;i < 10; i++){
+console.log (password);
+password = password + "def";
+}
+
+
+//console.log(getRandomInt(upperChar.length));
+
+//console.log(upperChar[getRandomInt(upperChar.length)]);
+
+//console.log(upperChar[2 * 12]);
+
+
+
+
+
+  //console.log(upperChar.length,upperChar[3])
+
+//create prompt with criteria - create button ---> "how many characters would you like your password to contain? Please choose between 8-128 characters"
+//function to prompt user for password criteria
+
+
+//function: prompt user for characters for amount of characters
+    // store prompt in varlength = prompt
+    //if statement for amount of characters less than 8 or greater than 128
+        //alert must be greater than 8 and less than 129
+
+
+
+
+
+
+
+
